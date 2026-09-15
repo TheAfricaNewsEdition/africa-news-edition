d5281d6f05297c7b671bfceb3c123d8b1e464a83
TheAfricaNewsEdition <osmansalman999@gmail.com>
2026-09-14 21:04:04 -0400

    Update posts.js
    added file for sept 15

--- posts.js
+++ posts.js
@@ -23,12 +23,20 @@
 
 window.NEWS_POSTS = [
    {
+  file: "the-africa-news-edition-sep15.html",
+  title: "September 15 - 48 Hour Regional Roundup",
+  date: "September 15, 2026",
+  category: "The last 48 Hours in Africa",
+  excerpt: "Character is like pregnancy; it cannot be hidden forever.",
+  featured: true
+},
+   {
   file: "the-africa-news-edition-sep10.html",
   title: "September 10 - 48 Hour Regional Roundup",
   date: "September 10, 2026",
   category: "The last 48 Hours in Africa",
   excerpt: "Even if you’re dancing in water, your enemies will still accuse you of…raising dust.",
-  featured: true
+  featured: false
 },
    {
   file: "the-africa-news-edition-sep7.html",
